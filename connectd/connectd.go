59421730d225a0584544a38046250909ac12741b
Luna <luna.dragon@suse.com>
2023-07-11 14:25:53 +0530

    add all exposed functions from connectd

--- connectd/connectd.go
+++ connectd/connectd.go
@@ -68,7 +68,7 @@ func Credentials(path string) string {
 }
 
 func (f Connectd) CreateCredentialsFile(login, password, token, path string) string {
-	if err:=connect.CreateCredentials(login, password, token, path); err!=nil {
+	if err := connect.CreateCredentials(login, password, token, path); err != nil {
 		return errorToJSON(err)
 	}
 	return "{}"
@@ -76,7 +76,253 @@ func (f Connectd) CreateCredentialsFile(login, password, token, path string) str
 
 func (f Connectd) CurlrcCredentials() string {
 	creds, _ := connect.ReadCurlrcCredentials()
-	jsn, _:= json.Marshal(creds)
+	jsn, _ := json.Marshal(creds)
+	return string(jsn)
+}
+
+func (f Connectd) ShowProduct(clientParams, product string) string {
+	loadConfig(clientParams)
+
+	var productQuery connect.Product
+	if err := json.Unmarshal([]byte(product), &productQuery); err != nil {
+		return errorToJSON(connect.JSONError{Err: err})
+	}
+	productData, err := connect.ShowProduct(productQuery)
+
+	if err != nil {
+		return errorToJSON(err)
+	}
+	jsn, err := json.Marshal(productData)
+	if err != nil {
+		return errorToJSON(err)
+	}
+	return string(jsn)
+}
+
+func (f Connectd) ActivateProduct(clientParams, product, email string) string {
+	loadConfig(clientParams)
+
+	var p connect.Product
+
+	if err := json.Unmarshal([]byte(product), &p); err != nil {
+		return errorToJSON(connect.JSONError{Err: err})
+	}
+
+	service, err := connect.ActivateProduct(p, email)
+	if err != nil {
+		return errorToJSON(err)
+	}
+	jsn, err := json.Marshal(service)
+	if err != nil {
+		return errorToJSON(err)
+	}
+	return string(jsn)
+}
+
+func ActivatedProducts(clientParams string) string {
+	loadConfig(clientParams)
+	products, err := connect.ActivatedProducts()
+	if err != nil {
+		return errorToJSON(err)
+	}
+	jsn, err := json.Marshal(products)
+	if err != nil {
+		return errorToJSON(err)
+	}
+	return string(jsn)
+}
+
+func (f Connectd) DeactivateProduct(clientParams, product string) string {
+	loadConfig(clientParams)
+	var p connect.Product
+
+	if err := json.Unmarshal([]byte(product), &p); err != nil {
+		return errorToJSON(connect.JSONError{Err: err})
+	}
+
+	service, err := connect.DeactivateProduct(p)
+	if err != nil {
+		return errorToJSON(err)
+	}
+	jsn, err := json.Marshal(service)
+	if err != nil {
+		return errorToJSON(err)
+	}
+	return string(jsn)
+}
+
+func (f Connectd) GetConfig(path string) string {
+	c := connect.NewConfig()
+	c.Path = path
+	c.Load()
+	jsn, err := json.Marshal(c)
+	if err != nil {
+		errorToJSON(err)
+	}
+	return string(jsn)
+}
+
+func (f Connectd) WriteConfig(clientParams string) string {
+	loadConfig(clientParams)
+	if err := connect.CFG.Save(); err != nil {
+		return errorToJSON(err)
+	}
+	return "{}"
+}
+
+func (f Connectd) Status(format string) (string, *dbus.Error) {
+	connect.CFG.Load()
+	output, err := connect.GetProductStatuses(format)
+
+	if err != nil {
+		return output, dbus.MakeFailedError(err)
+	}
+	return output, nil
+}
+
+func (f Connectd) UpdateCertificates() string {
+	if err := connect.UpdateCertificates(); err != nil {
+		return errorToJSON(err)
+	}
+	return "{}"
+}
+
+func (f Connectd) ReloadCertificates() string {
+	if err := connect.ReloadCertPool(); err != nil {
+		return errorToJSON(err)
+	}
+	return ""
+}
+
+func (f Connectd) ListInstallerUpdates(clientParams, product string) string {
+	loadConfig(clientParams)
+	var productQuery connect.Product
+	if err := json.Unmarshal([]byte(product), &productQuery); err != nil {
+		return errorToJSON(connect.JSONError{Err: err})
+	}
+	repos, err := connect.InstallerUpdates(productQuery)
+	if err != nil {
+		return errorToJSON(err)
+	}
+	jsn, err := json.Marshal(repos)
+	if err != nil {
+		return errorToJSON(err)
+	}
+	return string(jsn)
+}
+
+func (f Connectd) SystemMigrations(clientParams, products string) string {
+	loadConfig(clientParams)
+	installed := make([]connect.Product, 0)
+	if err := json.Unmarshal([]byte(products), &installed); err != nil {
+		return errorToJSON(connect.JSONError{Err: err})
+	}
+	migrations, err := connect.ProductMigrations(installed)
+	if err != nil {
+		return errorToJSON(err)
+	}
+	jsn, err := json.Marshal(migrations)
+	if err != nil {
+		return errorToJSON(err)
+	}
+	return string(jsn)
+}
+
+func (f Connectd) OFFlineSystemMigration(clientParams, products, targetBaseProduct string) string {
+	loadConfig(clientParams)
+	installed := make([]connect.Product, 0)
+	err := json.Unmarshal([]byte(products), &installed)
+	if err != nil {
+		return errorToJSON(connect.JSONError{Err: err})
+	}
+	var target connect.Product
+	if err := json.Unmarshal([]byte(targetBaseProduct), &target); err != nil {
+		return errorToJSON(connect.JSONError{Err: err})
+	}
+	migrations, err := connect.OfflineProductMigrations(installed, target)
+	if err != nil {
+		return errorToJSON(err)
+	}
+	jsn, err := json.Marshal(migrations)
+	if err != nil {
+		return errorToJSON(err)
+	}
+	return string(jsn)
+}
+
+func (f Connectd) UpdateProduct(clientParams, product string) string {
+	loadConfig(clientParams)
+
+	var prod connect.Product
+	err := json.Unmarshal([]byte(product), &prod)
+	if err != nil {
+		return errorToJSON(connect.JSONError{Err: err})
+	}
+	service, err := connect.UpgradeProduct(prod)
+	if err != nil {
+		return errorToJSON(err)
+	}
+	jsn, err := json.Marshal(service)
+	if err != nil {
+		return errorToJSON(err)
+	}
+	return string(jsn)
+}
+
+func (f Connectd) Synchronize(clientParams, products string) string {
+	loadConfig(clientParams)
+
+	prods := make([]connect.Product, 0)
+	err := json.Unmarshal([]byte(products), &prods)
+	if err != nil {
+		return errorToJSON(connect.JSONError{Err: err})
+	}
+	activated, err := connect.SyncProducts(prods)
+	if err != nil {
+		return errorToJSON(err)
+	}
+	jsn, err := json.Marshal(activated)
+	if err != nil {
+		return errorToJSON(err)
+	}
+	return string(jsn)
+}
+
+func (f Connectd) SystemActivations(clientParams string) string {
+	loadConfig(clientParams)
+
+	// converting from map to list as expected by Ruby clients
+	actList := make([]connect.Activation, 0)
+	actMap, err := connect.SystemActivations()
+	if err != nil {
+		return errorToJSON(err)
+	}
+	for _, a := range actMap {
+		actList = append(actList, a)
+	}
+	jsn, err := json.Marshal(actList)
+	if err != nil {
+		return errorToJSON(err)
+	}
+	return string(jsn)
+}
+
+func (f Connectd) SearchPackage(clientParams, product, query string) string {
+	loadConfig(clientParams)
+
+	var p connect.Product
+	err := json.Unmarshal([]byte(product), &p)
+	if err != nil {
+		return errorToJSON(connect.JSONError{Err: err})
+	}
+	results, err := connect.SearchPackage(query, p)
+	if err != nil {
+		return errorToJSON(err)
+	}
+	jsn, err := json.Marshal(results)
+	if err != nil {
+		return errorToJSON(err)
+	}
 	return string(jsn)
 }
 
@@ -88,16 +334,6 @@ func (f Connectd) Version(fullVersion bool) (string, *dbus.Error) {
 		version = connect.GetShortenedVersion()
 	}
 	return version, nil
-}
-
-func (f Connectd) Status(format string) (string, *dbus.Error) {
-	connect.CFG.Load()
-	output, err := connect.GetProductStatuses(format)
-
-	if err != nil {
-		return output, dbus.MakeFailedError(err)
-	}
-	return output, nil
 }
 
 func main() {
--- connectd/intro.go
+++ connectd/intro.go
@@ -33,12 +33,82 @@ const intro = `
 		<method name="CurlrcCredentials">
 			<arg direction="out" type="s"/>
 		</method>
-		<method name="Version">
-			<arg direction="in" type="b"/>
+		<method name="ShowProduct">
+			<arg direction="in" type="s"/>
+			<arg direction="in" type="s"/>
+			<arg direction="out" type="s"/>
+		</method>
+		<method name="ActivateProduct">
+			<arg direction="in" type="s"/>
+			<arg direction="in" type="s"/>
+			<arg direction="in" type="s"/>
+			<arg direction="out" type="s"/>
+		</method>
+		<method name="ActivatedProducts">
+			<arg direction="in" type="s"/>
+			<arg direction="out" type="s"/>
+		</method>
+		<method name="DeactivateProduct">
+			<arg direction="in" type="s"/>
+			<arg direction="in" type="s"/>
+			<arg direction="out" type="s"/>
+		</method>
+		<method name="GetConfig">
+			<arg direction="in" type="s"/>
+			<arg direction="out" type="s"/>
+		</method>
+		<method name="WriteConfig">
+			<arg direction="in" type="s"/>
 			<arg direction="out" type="s"/>
 		</method>
 		<method name="Status">
 			<arg direction="in" type="s" />
 			<arg direction="out" type="s" />
+		</method>
+		<method name="UpdateCertificates">
+			<arg direction="out" type="s" />
+		</method>
+		<method name="ReloadCertificates">
+			<arg direction="out" type="s" />
+		</method>
+		<method name="ListInstallerUpdates">
+			<arg direction="in" type="s" />
+			<arg direction="in" type="s" />
+			<arg direction="out" type="s" />
+		</method>
+		<method name="SystemMigrations">
+			<arg direction="in" type="s" />
+			<arg direction="in" type="s" />
+			<arg direction="out" type="s" />
+		</method>
+		<method name="OFFlineSystemMigration">
+			<arg direction="in" type="s" />
+			<arg direction="in" type="s" />
+			<arg direction="in" type="s" />
+			<arg direction="out" type="s" />
+		</method>
+		<method name="UpdateProduct">
+			<arg direction="in" type="s" />
+			<arg direction="in" type="s" />
+			<arg direction="out" type="s" />
+		</method>
+		<method name="Synchronize">
+			<arg direction="in" type="s" />
+			<arg direction="in" type="s" />
+			<arg direction="out" type="s" />
+		</method>
+		<method name="SearchPackage">
+			<arg direction="in" type="s" />
+			<arg direction="out" type="s" />
+		</method>
+		<method name="OFFlineSystemMigration">
+			<arg direction="in" type="s" />
+			<arg direction="in" type="s" />
+			<arg direction="in" type="s" />
+			<arg direction="out" type="s" />
+		</method>
+		<method name="Version">
+			<arg direction="in" type="b"/>
+			<arg direction="out" type="s"/>
 		</method>
 	</interface>` + introspect.IntrospectDataString + `</node>`
